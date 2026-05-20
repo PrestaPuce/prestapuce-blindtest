904e202946c865aea6aa59db14ba5cd3ccd5dcaf
PrestaPuce <contact@prestapuce.com>
2026-05-20 08:55:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const APP_CONFIG = {
+    // 🖼️ IMAGES ET LOGOS
+    LOGO_URL: "https://static.wixstatic.com/media/eda9e4_e7ab6af2d5a7428f9bc7e4af454e20ef~mv2.png", 
+    LOGO_BUZTAPUCE: "https://static.wixstatic.com/media/eda9e4_dc2ff731fcff46ae864a10091fbab8c7~mv2.png",
+    
+    // 🎵 SONS
+    SOUND_BUZZ: "https://static.wixstatic.com/mp3/eda9e4_b0c741f047f549a28051b8a2a2264f37.wav", 
+    SOUND_CHAMPION: "https://static.wixstatic.com/mp3/eda9e4_f7bb386482f54a2fb8f00a0c4ebe990d.mp3",
+    SOUND_APPLAUSE: "https://static.wixstatic.com/mp3/eda9e4_a38a05892581432e9304fa03888211e3.mp3"
+};
+
+// ANCIEN LIEN https://static.wixstatic.com/media/eda9e4_20fab3d00cf94a9987b925f3cee8e2ce~mv2.png
